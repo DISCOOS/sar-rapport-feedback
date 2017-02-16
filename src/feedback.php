6d0bd83b33f1796ddd5b1f46fdc55805c0b56908
Kenneth Gulbrandsøy <kenneth.gulbrandsoy@gmail.com>
2017-02-16 10:39:28 +0100

    Fixed href in feedback

--- src/feedback.php
+++ src/feedback.php
@@ -82,15 +82,16 @@ if($auth = taiga_login()) {
             if ($issue) {
 
                 $ref = $issue['ref'];
+		$href = "https://tree.taiga.io/project/rge-sar-rapport/issue/$href"
 
                 if($id) {
                     $comments = taiga_get_issue_comments($auth, $issue['id']);
-		    $result = 'Takk! <a href="' . $ref . '">Tilbakemelding ' . $ref . '</a> er registrert. ';
+		    $result = 'Takk! <a href="' . $href . '">Tilbakemelding ' . $href . '</a> er registrert. ';
                     $result .= 'Vi vil ta kontakt når din tilbakemelding er behandlet.';
 
                 } else {
-                    $result = 'Takk! <a href="' . $ref . '">Tilbakemelding ' . $ref . '</a> er registrert. ';
-                    if(notify('Tilbakemelding ' . $ref . ' er registrert', $email, $name, $result)) {
+                    $result = 'Takk! <a href="' . $href . '">Tilbakemelding ' . $href . '</a> er registrert. ';
+                    if(notify('Tilbakemelding ' . $href . ' er registrert', $email, $name, $result)) {
                         $result .= 'Kvittering er sendt til ' . $email . '. ';
                     }
                     $result .= 'Vi vil ta kontakt når din tilbakemelding er behandlet.';
